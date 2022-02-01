25b381423a6ec51422bb5ceca8fe1919ec2e64e3
xventrux <vvyacheslavx@gmail.com>
2022-02-01 20:40:05 +0500

    Добавил консольное приложение для создания миграций

---
+++ TGBot.Migrations/MigrationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TGBot.DataAccess;
+
+namespace TGBot.Migrations
+{
+    public class MigrationDbContext : BaseDbContext
+    {
+        public MigrationDbContext(DbContextOptions<MigrationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ TGBot.Migrations/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TGBot.Migrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
