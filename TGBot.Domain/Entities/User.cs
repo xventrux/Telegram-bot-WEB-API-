ed024164bb647f63408c29c4191af5797fd1e332
xventrux <vvyacheslavx@gmail.com>
2022-02-01 20:13:55 +0500

    Описал класс пользователя

---
+++ TGBot.Domain/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TGBot.Domain.Entities
+{
+    /// <summary>
+    /// Пользователь
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Идентификатор пользователя
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Псевдоним пользователя
+        /// </summary>
+        public string UserName { get; set; }
+    }
+}
