339d42fc2bb1db439eed405a9150dc066760b5ea
xventrux <vvyacheslavx@gmail.com>
2022-02-01 21:09:39 +0500

    Добавил класс для соединения с телеграмм клиентом

---
+++ TGBot/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace TGBot
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddTelegramBotClient(this IServiceCollection servicesCollection,
+            IConfiguration configuration)
+        {
+            var client = new TelegramBotClient(configuration["Token"]);
+            var webHook = $"{configuration["Url"]}/api/message/update";
+            client.SetWebhookAsync(webHook).Wait();
+
+            return servicesCollection.AddTransient<ITelegramBotClient>(x => client);
+        }
+    }
+}
