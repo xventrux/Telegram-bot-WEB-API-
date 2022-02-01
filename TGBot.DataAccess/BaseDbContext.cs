fff28cdca45209a090195a0dd48c24849ff90311
xventrux <vvyacheslavx@gmail.com>
2022-02-01 20:21:21 +0500

    Описал класс конфигурации пользователя

---
+++ TGBot.DataAccess/BaseDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TGBot.DataAccess.Configurations;
+
+namespace TGBot.DataAccess
+{
+    public class BaseDbContext : DbContext
+    {
+        public BaseDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+    }
+}
